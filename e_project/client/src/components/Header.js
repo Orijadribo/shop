812be9725385eaa40631ed6f2289d35e8ac181e7
Daniel Orija <danielorijadribo@gmail.com>
2023-03-27 03:55:54 +0300

    New

--- e_project/client/src/components/Header.js
+++ e_project/client/src/components/Header.js
@@ -20,8 +20,8 @@ const Header = () => {
                         <li className='text-base text-black font-bold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300'>Blog</li>
                     </ul>
                     <div className='relative'>
-                        <HiOutlineShoppingBag className='w-full h-[35px] '/>
-                        <span className='absolute w-8 top-3 left-0 text-sm flex items-center justify-center font-semibold'>0</span>
+                        <HiOutlineShoppingBag className='w-full h-[40px] '/>
+                        <span className='absolute w-8 top-4 left-1 text-sm flex items-center justify-center font-semibold'>0</span>
                     </div>
                     <img className='w-8 h-8 rounded-full'
                         src='https://images.pexels.com/photos/264547/pexels-photo-264547.jpeg? auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='userlogo'
